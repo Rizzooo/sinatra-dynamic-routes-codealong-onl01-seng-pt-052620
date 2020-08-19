c038c9563f06505aaf5234823c5225ec23800942
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-08-19 14:29:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,7 +21,9 @@ class App < Sinatra::Base
   end
   
   get "/multiply/:num1/:num2" do 
-    params[:num1].to_i * params[:num2].to_i
+    @answer = params[:num1].to_i * params[:num2].to_i
+    
+    "#{answer}"
   end
 
 end
